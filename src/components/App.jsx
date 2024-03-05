ef647a79c9e8bef899057f1b1c6e4ddb7fe9161b
gorniakewa300 <132227665+gorniakewa300@users.noreply.github.com>
2024-03-05 22:53:31 +0100

    final version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,24 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const stateContactsArr = JSON.stringify(this.state.contacts);
+    const newContactsArr = JSON.stringify(prevState.contacts);
+    
+    if (stateContactsArr !== newContactsArr) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   formSubmitHandler = data => {
     this.repeatControl(data);
   };
